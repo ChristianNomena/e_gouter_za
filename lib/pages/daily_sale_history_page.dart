95704132ea4aec9e70e36794e0aa5ee5ad991d07
ChristianNomena <christiangrzk@gmail.com>
2022-03-11 13:59:05 +0300

    add dynamic history daily sale

--- lib/pages/daily_sale_history_page.dart
+++ lib/pages/daily_sale_history_page.dart
@@ -59,63 +59,93 @@ class _DailySaleHistoryPageState extends State<DailySaleHistoryPage> {
       ),
       drawer: const AppDrawerMenu(),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ListView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemCount: dailySaleList.length,
+              itemBuilder: (context, index) {
+                return _getHistoryDailySale(dailySale: dailySaleList[index]);
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _getHistoryDailySale({required DailySale dailySale}) {
+    return Column(
+      children: [
+        Card(
+          elevation: 12.0,
           child: Column(
             children: [
-              Card(
-                elevation: 12.0,
-                child: Column(
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      height: 40.0,
-                      color: kThirdColor,
-                      alignment: Alignment.center,
-                      child: const Text(
-                        "10 mars 2022",
-                        style: TextStyle(
-                          color: kLightColor,
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    ),
-                    const ListTile(
-                      leading: Text("Happy King"),
-                      trailing: Text("50 000 Ar"),
-                    ),
-                  ],
+              Container(
+                width: double.infinity,
+                height: 40.0,
+                color: kThirdColor,
+                alignment: Alignment.center,
+                child: Text(
+                  dailySale.date,
+                  style: const TextStyle(
+                    color: kLightColor,
+                    fontSize: 20.0,
+                  ),
                 ),
               ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              Card(
-                elevation: 12.0,
-                child: Column(
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      height: 40.0,
-                      color: kThirdColor,
-                      alignment: Alignment.center,
-                      child: const Text(
-                        "10 mars 2022",
-                        style: TextStyle(
-                          color: kLightColor,
-                          fontSize: 20.0,
-                        ),
-                      ),
-                    ),
-                    const ListTile(
-                      leading: Text("Happy King"),
-                      trailing: Text("50 000 Ar"),
-                    ),
-                  ],
+              const ListTile(
+                leading: Text(
+                  "Restaurant",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
+                trailing: Text(
+                  "Total",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const Divider(),
+              ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: dailySale.incomes.length,
+                itemBuilder: (context, index) {
+                  return _getHistoryDailySaleIncome(
+                      income: dailySale.incomes[index]);
+                },
               ),
             ],
           ),
+        ),
+        const SizedBox(
+          height: 20.0,
+        ),
+      ],
+    );
+  }
+
+  _getHistoryDailySaleIncome({required Income income}) {
+    return ListTile(
+      leading: Text(
+        income.restaurant,
+        style: const TextStyle(
+          color: kSoftDarkColor,
+        ),
+      ),
+      trailing: Text(
+        income.total,
+        style: const TextStyle(
+          color: kSoftDarkColor,
         ),
       ),
     );
